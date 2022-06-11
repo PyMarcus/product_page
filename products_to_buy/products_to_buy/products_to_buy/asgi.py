dc986b0ee28b340cee6085759b49e5bd2f749e3a
Marcus <88283829+PyMarcus@users.noreply.github.com>
2022-06-11 16:02:52 -0300

    Update asgi.py

--- products_to_buy/products_to_buy/products_to_buy/asgi.py
+++ products_to_buy/products_to_buy/products_to_buy/asgi.py
@@ -1,9 +1,5 @@
 """
-ASGI config for products_to_buy project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
